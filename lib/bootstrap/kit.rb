9a13d0b3ca495d22d7c8e0f4176aae2b10779f55
Mohd Khairi Mohd Adnan <khairi@labs.my>
2017-03-07 20:29:58 +0800

    born

---
+++ lib/bootstrap/kit.rb
@@ -0,0 +1,7 @@
+require "bootstrap/kit/version"
+
+module Bootstrap
+  module Kit
+    # Your code goes here...
+  end
+end
---
+++ lib/bootstrap/kit/version.rb
@@ -0,0 +1,5 @@
+module Bootstrap
+  module Kit
+    VERSION = "0.1.0"
+  end
+end
